320d120f079e25a16577c578af09599e6e53033d
fredrikjohanssonnn <fjohansson.creative@gmail.com>
2022-07-13 22:47:18 +0200

    Added remove method and passed it to the button

--- src/components/Track/Track.js
+++ src/components/Track/Track.js
@@ -6,14 +6,19 @@ export default class Track extends Component {
   constructor(props){
     super(props)
     this.addTrack = this.addTrack.bind(this)
+    this.removeTrack = this.removeTrack.bind(this)
   }
 
   addTrack(){
     this.props.onAdd = this.props.track
   }
 
+  removeTrack(){
+    this.props.onRemove = this.props.track
+  }
+
   renderAction(){
-    this.props.isRemoval ? <button className="Track-action">-</button> : <button onClick={this.addTrack} className="Track-action">+</button>
+    this.props.isRemoval ? <button onClick={this.removeTrack} className="Track-action">-</button> : <button onClick={this.addTrack} className="Track-action">+</button>
   }
 
   render() {
