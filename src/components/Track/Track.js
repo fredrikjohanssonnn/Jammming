20afeedc62c35485760d10ba84fdf32e7fb75f09
fredrikjohanssonnn <fjohansson.creative@gmail.com>
2022-07-13 22:42:36 +0200

    Added methods to lift up the state and add/remove the current track

--- src/components/Track/Track.js
+++ src/components/Track/Track.js
@@ -2,14 +2,28 @@ import React, { Component } from 'react'
 import './Track.css'
 
 export default class Track extends Component {
+
+  constructor(props){
+    super(props)
+    this.addTrack = this.addTrack.bind(this)
+  }
+
+  addTrack(){
+    this.props.onAdd = this.props.track
+  }
+
+  renderAction(){
+    this.props.isRemoval ? <button className="Track-action">-</button> : <button onClick={this.addTrack} className="Track-action">+</button>
+  }
+
   render() {
     return (
       <div className="Track">
         <div className="Track-information">
-          <h3></h3>
+          <h3>{this.props.track.name}</h3>
           <p></p>
         </div>
-        <button className="Track-action"></button>
+        
       </div>
     )
   }
