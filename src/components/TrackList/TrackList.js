a31941069f8887bcc12edd95f5c7da4231fc3255
fredrikjohanssonnn <fjohansson.creative@gmail.com>
2022-07-13 22:43:11 +0200

    Map through the array of tracks. Passing the props from the track to the Track-component

--- src/components/TrackList/TrackList.js
+++ src/components/TrackList/TrackList.js
@@ -1,12 +1,16 @@
 import React, { Component } from 'react'
 import './TrackList.css'
+import Track from '../Track/Track'
 
 export default class TrackList extends Component {
   render() {
+    
     return (
       <div className="TrackList">
-   
-</div>
+        {this.props.tracks.map(track => (
+          <Track onRemove={this.props.onRemove} isRemoval={this.props.isRemoval} key={track.id} track={track} />
+        ))}
+      </div>
     )
   }
 }
