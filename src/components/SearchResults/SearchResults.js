c678635fea03f851c249a45976cd82e53e925966
fredrikjohanssonnn <fjohansson.creative@gmail.com>
2022-07-13 22:41:53 +0200

    Passing methods from the App.js, further down

--- src/components/SearchResults/SearchResults.js
+++ src/components/SearchResults/SearchResults.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react'
 import './SearchResults.css'
+import TrackList from '../TrackList/TrackList'
 
 export default class SearchResults extends Component {
   render() {
     return (
       <div className="SearchResults">
         <h2>Results</h2>
+        <TrackList isRemoval={false} onAdd={this.props.onAdd} tracks={this.props.results} />
       </div>
     )
   }
