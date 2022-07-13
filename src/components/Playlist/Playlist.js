1741162a1381421fb315312466fa324bcff7f2d4
fredrikjohanssonnn <fjohansson.creative@gmail.com>
2022-07-13 22:41:35 +0200

    Passing methods from the App.js, further down

--- src/components/Playlist/Playlist.js
+++ src/components/Playlist/Playlist.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react'
 import './Playlist.css'
+import TrackList from '../TrackList/TrackList'
 
 export default class Playlist extends Component {
   render() {
     return (
       <div className="Playlist">
         <input defaultValue={'New Playlist'}/>
+        <TrackList onRemoval={true} onRemove={this.props.onRemove} tracks={this.props.tracks} />
         <button className="Playlist-save">SAVE TO SPOTIFY</button>
       </div>
     )
