08bafa539e89bf41666d2b774394ff68caa8d440
fredrikjohanssonnn <fjohansson.creative@gmail.com>
2022-07-13 22:41:04 +0200

    Added some default state and created two methods to both add tracks and remove tracks. They are passed down as props

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,18 +1,54 @@
 import React, { Component } from 'react'
 import './App.css'
+import SearchResults from '../SearchResults/SearchResults'
+import PlayList from '../Playlist/Playlist'
 
 export default class App extends Component {
-  render() {
-    return (
-        <div>
-        <h1>Ja<span className="highlight">mmm</span>ing</h1>
-        <div className="App">
 
-            <div className="App-playlist">
+    constructor(props){
+        super(props)
+        this.state = {
+            searchResults: [],
+            playlistName: 'Summer',
+            playlistTracks: [
+                {
+                    id: 1,
+                    name: 'Rullar fram',
+                    artist: 'Fronda',
+                    album: 'Fridlysta Frekvenser'
+                }
+            ]
+        }
+        this.addTrack = this.addTrack.bind(this)
+        this.removeTrack = this.removeTrack.bind(this)
+    }
+    
+    addTrack(track){
+        if (this.state.playlistTracks.some(e => e.id !== track.id)){
+            const newArr = this.state.playlistTracks.push(track)
+            this.setState({ playlistTracks: newArr })
+        } else {
+            return
+        }
+    }
 
+    removeTrack(track){
+        const newArr = this.state.playlistTracks.filter(e => e.id !== track.id)
+        this.setState({ playlistTracks: newArr })
+    }
+
+    render() {
+        return (
+            <div>
+            <h1>Ja<span className="highlight">mmm</span>ing</h1>
+            <div className="App">
+
+                <div className="App-playlist">
+                    <SearchResults onAdd={this.addTrack} results={this.state.searchResults} />
+                    <PlayList onRemove={this.removeTrack} name={this.state.playlistName} tracks={this.state.playlistTracks} />
+                </div>
             </div>
         </div>
-    </div>
-    )
-  }
+        )
+    }
 }
